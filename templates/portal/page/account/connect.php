bd488fa429ac82fcdc60a308b354b273d96acc3e
EA <arrive@free.fr>
2020-06-12 15:33:57 +0200

    set default actions in account connect

--- templates/portal/page/account/connect.php
+++ templates/portal/page/account/connect.php
@@ -1,6 +1,6 @@
 <?php $this->render('/portal/page/account/fields/login'); ?>
 <?php $this->render('/portal/page/account/fields/password'); ?>
-<?php foreach ($models['actions'] as $action) { ?>
+<?php foreach ($models['actions'] ?? Account::actions(false) as $action) { ?>
 <?php   if ($action !== 'connect') { ?>
 <div class="switch" data-action="<?php echo $action; ?>"><?php echo $locale->switch->$action; ?></div>
 <?php   } ?>
