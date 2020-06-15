81c646e9482eb7cbf9c09f68370f41c729da2a2d
EA <arrive@free.fr>
2020-06-15 16:14:37 +0200

    remove debug log

--- classes/modules/Account.php
+++ classes/modules/Account.php
@@ -107,8 +107,6 @@ class Account extends Module {
             return $this->redirect($this->baseURL.'/connect');
         }
         list($models, $update) = $this->userdata();
-        Zord::log($models);
-        Zord::log($update);
         if ($update) {
             $data = $this->check($models, Zord::value('account', ['properties',$scope]));
             if (is_string($data)) {
