9f133335d310316d28d7bb2c4c3cdcc9e58eeeb3
EA <arrive@free.fr>
2020-06-25 16:27:23 +0200

    ImageResize for min or max size

--- classes/tools/ImageResize.php
+++ classes/tools/ImageResize.php
@@ -2,27 +2,33 @@
 
 class ImageResize extends ImageProcessor {
     
-    public $name = null;
+    public $name  = null;
+    public $limit = 'max';
 
     public function run() {
         $path = $this->folder.$this->name.'.'.$this->format;
+        $widthRatio  = 1;
+        $heightRatio = 1;
         switch ($this->width <=> $this->height) {
-            case 0: {
-                $width  = $this->size;
-                $height = $this->size;
-                break;
-            }
             case 1: {
-                $height = (int) ($this->height * ($this->size / $this->width));
-                $width  = $this->size;
+                if ($this->limit === "max") {
+                    $heightRatio = $this->height / $this->width;
+                } else if ($this->limit === "min") {
+                    $widthRatio = $this->width / $this->height;
+                }
                 break;
             }
             case -1: {
-                $width  = (int) ($this->width * ($this->size / $this->height));
-                $height = $this->size;
+                if ($this->limit === "max") {
+                    $widthRatio = $this->width / $this->height;
+                } else if ($this->limit === "min") {
+                    $heightRatio = $this->height / $this->width;
+                }
                 break;
             }
         }
+        $width  = (int) ($this->size * $widthRatio);
+        $height = (int) ($this->size * $heightRatio);
         switch ($this->processor) {
             case 'Imagick': {
                 $thumb = clone $this->image;
