e27243a0c863a7956d01af9f699300f6467900c0
EA <arrive@free.fr>
2022-12-08 11:50:41 +0100

    Zord::execute -> popen mode 'r'

--- classes/Zord.php
+++ classes/Zord.php
@@ -532,7 +532,7 @@ class Zord {
 	            break;
 	        }
 	        case 'popen': {
-	            $proc = popen($command, $params['mode'] ?? 'w');
+	            $proc = popen($command, $params['mode'] ?? 'r');
 	            if (is_resource($proc)) {
 	                $result = stream_get_contents($proc);
 	                pclose($proc);
