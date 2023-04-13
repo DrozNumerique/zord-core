ed053dcc29ddcf28f5078a8c0e8c864f660453db
EA <arrive@free.fr>
2023-04-13 21:53:48 +0200

    add cache

---
+++ classes/tools/Cache.php
@@ -0,0 +1,32 @@
+<?php
+use \Cache\Adapter\Filesystem\FilesystemCachePool;
+use \Cache\Prefixed\PrefixedCachePool;
+use \League\Flysystem\Adapter\Local;
+use \League\Flysystem\Filesystem;
+
+class Cache {
+    
+    private static function getPool($type) {
+        return new FilesystemCachePool(new Filesystem(new Local('/tmp')), 'cache'.DS.$type);
+    }
+    
+    public static function setItem($type, $key, $value) {
+        $item = self::getPool($type)->getItem($key)->set($value);
+        self::getPool($type)->save($item);
+    }
+    
+    public static function getItem($type, $key) {
+        return self::getPool($type)->getItem($key)->get();
+    }
+    
+    public static function hasItem($type, $key) {
+        return self::getPool($type)->hasItem($key);
+    }
+    
+    public static function deleteItem($type, $key) {
+        self::getPool($type)->deleteItem($key);
+    }
+
+}
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,87 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '28c9e96b25ea41aa1ae9c5bedff9a1b8b1aead4b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '28c9e96b25ea41aa1ae9c5bedff9a1b8b1aead4b',
+    ),
+    'cache/cache' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ca3bd08ebe53f5b13b5c4f589d57b1fe97da001c',
+    ),
+    'doctrine/cache' => 
+    array (
+      'pretty_version' => '1.13.0',
+      'version' => '1.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '56cd022adb5514472cb144c087393c1821911d09',
+    ),
+    'league/flysystem' => 
+    array (
+      'pretty_version' => '1.1.10',
+      'version' => '1.1.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3239285c825c152bcc315fe0e87d6b55f5972ed1',
+    ),
+    'league/mime-type-detection' => 
+    array (
+      'pretty_version' => '1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ff6248ea87a9f116e78edd6002e39e5128a0d4dd',
+    ),
+    'psr/cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd11b50ad223250cf17b86e38383413f5a6764bf8',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+  ),
+);
