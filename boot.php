bb134a8ab8c039c2415ffd6e22338264274b2336
EA <arrive@free.fr>
2023-05-13 20:44:06 +0200

    improve boot.php

--- boot.php
+++ boot.php
@@ -1,17 +1,17 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
-$root = null;
-$here = 'NULL';
-if (isset($_SERVER['PWD'])) {
-    $root = dirname($_SERVER['PHP_SELF']);
-    if ($root === '.') {
-        $root = $_SERVER['PWD'];
+if (!defined('ROOT')) {
+    if (isset($_SERVER['PWD'])) {
+        $root = dirname($_SERVER['PHP_SELF']);
+        if ($root === '.') {
+            $root = $_SERVER['PWD'];
+        }
+    } else if (isset($_SERVER['DOCUMENT_ROOT'])) {
+        $root = $_SERVER['DOCUMENT_ROOT'];
+        $root = substr($root, 0, strpos($root, '/web'));
     }
-} else if (isset($_SERVER['DOCUMENT_ROOT'])) {
-    $root = $_SERVER['DOCUMENT_ROOT'];
-    $root = substr($root, 0, strpos($root, '/web'));
+    define('ROOT', $root.DS);
 }
-define('ROOT', $root.DS);
 $folders = [];
 if (file_exists(ROOT.'components') && is_dir(ROOT.'components')) {
     $folders[] = ROOT;
