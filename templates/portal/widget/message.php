b555c82f97a13d104633da5f61d7863394316b51
EA <arrive@free.fr>
2024-02-20 14:20:58 +0100

    cosmetic

--- templates/portal/widget/message.php
+++ templates/portal/widget/message.php
@@ -1,6 +1,6 @@
 <?php     foreach (explode('|', $message) as $_message) { ?>
 <?php       $__message = explode('=', $_message); ?>	
-<?php       foreach (explode('§', $__message[1]) as $___message) {?>
+<?php       foreach (explode('§', $__message[1]) as $___message) { ?>
 			<div class="message <?php echo $__message[0]; ?>"><?php echo $___message; ?></div>
-			}
+<?php       } ?>
 <?php     } ?>
