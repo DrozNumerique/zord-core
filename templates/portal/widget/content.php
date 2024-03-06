e5e0f2aa52563d26fe7c06cb814fd4407c6422b3
EA <arrive@free.fr>
2024-03-06 12:29:11 +0100

    cosmetic

--- templates/portal/widget/content.php
+++ templates/portal/widget/content.php
@@ -1,6 +1,6 @@
 <?php if (!empty($content) || ($editable ?? false)) { ?>
-<div class="content widget <?php echo $editable ? 'editable' : ''; ?>" data-name="<?php echo $name; ?>">
-	<div id="<?php echo $id ?>" class="content display<?php echo implode(' ', $class ?? []); ?>"><?php echo !empty($content) ? Zord::md2html($content) : $holder; ?></div>
+<div class="content widget<?php echo $editable ? ' editable' : ''; ?><?php echo !empty($class) ? ' '.implode(' ', $class) : ''; ?>" data-name="<?php echo $name; ?>">
+	<div id="<?php echo $id ?>" class="content display<?php echo !empty($class) ? ' '.implode(' ', $class) : ''; ?>"><?php echo !empty($content) ? Zord::md2html($content) : $holder; ?></div>
 <?php   if ($editable) { ?>
     <div class="content editor">
     	<textarea class="content text"><?php echo $content ?? ''; ?></textarea>
